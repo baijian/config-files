7f2cd6bd99df1fcf20fa8414a9cdc0d1d067a6e1
柏健 <jian.baij@gmail.com>
2013-07-29 09:27:19 +0800

    modify tweak tool name

--- environment/init.sh
+++ environment/init.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install openjdk-7-jdk ant aptitude apt-file bison chromium-browser codeblocks git subversion dconf-tools fcitx gimp gnome-tweak-tools gnupg curl vim meld libncurses5-dev libneon27 libneon27-gnutls libneon27-dev openssh-server libsqlite3-dev sqlite3 sqliteman tmux tnef uget unrar
+sudo apt-get install openjdk-7-jdk ant aptitude apt-file bison chromium-browser codeblocks git subversion dconf-tools fcitx gimp gnome-tweak-tool gnupg curl vim meld libncurses5-dev libneon27 libneon27-gnutls libneon27-dev openssh-server libsqlite3-dev sqlite3 sqliteman tmux tnef uget unrar
 echo "done."
 
