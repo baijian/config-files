4db50d129bc468885c4e9f3aa4b017ec8d72c5eb
柏健 <jian.baij@gmail.com>
2013-11-22 10:13:55 +0800

    modify tunnel shell script

--- mac/crontab/tunnel.sh
+++ mac/crontab/tunnel.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+lsof -iTCP -sTCP:LISTEN -P | grep ':7070' | grep -v 'grep' | awk '{print $2}' | xargs kill -9
+
 a=`lsof -iTCP -sTCP:LISTEN -P | grep ':7070' | grep -v 'grep' | wc -l`
 
 if [ $a == 0 ]; then
